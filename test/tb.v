fdc517812e65b47d14cefa2ee71ea4a591e95a06
NeoN <138122881+NeoN1015@users.noreply.github.com>
2026-09-19 14:18:19 +0800

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -4,7 +4,7 @@
 /* This testbench just instantiates the module and makes some convenient wires
    that can be driven / tested by the cocotb test.py.
 */
-module tb ();
+module tb;
 
   // Dump the signals to a FST file. You can view it with gtkwave or surfer.
   initial begin
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_example user_project (
+  tt_um_freq_counter user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
